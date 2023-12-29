b7caf4f6cbed5bd4fb9fcd188d2c3d02a9d5f05e
ragul0001 <ragulkumar0244@gmail.com>
2023-12-29 11:53:21 +0530

    first

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -20,20 +20,36 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container mx-auto max-w-7xl px-8">
-      <div className="flex justify-center items-center">
-        <div
-          className={`bg-blue-500 ${
-            isLandscape ? 'w-[500px] h-auto' : 'w-auto h-auto'
-          }`}
-        >
-          <img
-            alt=""
-            src={image}
-            className={`w-full h-full ${
-              isLandscape ? 'object-cover ' : 'object-contain'
-            }`}
-          />
+    <div className="container mx-auto max-w-7xl px-8 ">
+      <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
+        <div  className={`bg-blue-500 ${ isLandscape ? 'w-[500px] h-auto' : 'w-auto h-auto'}`}>
+              <div className={`w-full h-full relative bg-slate-700 ${isLandscape ? 'object-cover ' : 'object-contain'}`}>
+                          <div className=' text-white '>
+                                 <div className='p-6'>
+                                   <form>
+                                    <div className="mb-4">
+                                      <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">Name</label>
+                                      <input type="text" id="name" name="name" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300" placeholder="Your Name" required/>
+                                    </div>
+
+                                    <div className="mb-4">
+                                      <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
+                                      <input type="email" id="email" name="email" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300" placeholder="Your Email" required/>
+                                    </div>
+
+                                    <div className="mb-4">
+                                      <label htmlFor="message" className="block text-gray-700 text-sm font-bold mb-2">Message</label>
+                                      <textarea id="message" name="message" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300" placeholder="Your Message"  required></textarea>
+                                    </div>
+
+                                    <div className="flex justify-end">
+                                      <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">Submit</button>
+                                    </div>
+
+                                    </form>
+                                 </div>
+                          </div>
+              </div>
         </div>
       </div>
     </div>
